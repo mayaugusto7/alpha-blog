cf987cb17da060a3d614ba80a942472ffceae5a2
MAYCON RIBEIRO <maycon_ribeiro@hotmail.com>
2019-07-28 17:53:00 +0000

    extract redundacy method production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:  [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -21,12 +22,10 @@ class ArticlesController < ApplicationController
 
 
   def edit
-    @article = Article.find(params[:id])
   end
 
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Articles was successfully updated"
       redirect_to article_path(@article)
@@ -37,18 +36,20 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
