6d24ebc5ca0a137bf03256dfc3db72108aeef0c5
MAYCON RIBEIRO <maycon_ribeiro@hotmail.com>
2019-08-11 15:04:49 +0000

    Signup user alpha blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
   get 'pages/about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources 'users', except: [:new]
 
 end
