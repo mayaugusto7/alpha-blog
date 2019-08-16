5163e438a668c4b111c8b5e60e80ddbed40844b7
MAYCON RIBEIRO <maycon_ribeiro@hotmail.com>
2019-08-15 21:44:58 -0300

    Login and Logout sessions implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'users/signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -10,5 +11,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources 'users', except: [:new]
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
