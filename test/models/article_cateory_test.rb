fae0b33d0366bbe7383b75bdeeb16726bbb8aef9
MAYCON RIBEIRO <maycon_ribeiro@hotmail.com>
2019-08-31 10:34:18 -0300

    Many to Many associations category and articles

---
+++ test/models/article_cateory_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ArtcileCategoriesTest < Test::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    pend 'Not implemented'
+  end
+end
